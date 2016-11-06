30849532852913f077b00318734ef5909cb21128
vsuha <vsuha@Victorias-MBP.fios-router.home>
2016-11-06 18:06:40 -0500

    OSC help files, OSCClient, OSCServer max patch
    I added the maxmsp OSC help files, created a python OSCClient file to
    send data to max, and a max patch to read the data being sent

---
+++ OSC/OSCClient.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  6 16:50:53 2016
+
+@author: vsuha
+"""
+
+#there are a lot of different python-osc modules...I used pyosc
+#https://github.com/ptone/pyosc
+#pip install pyosc
+
+import OSC
+c = OSC.OSCClient()
+c.connect(('127.0.0.1', 57120)) 
+
+#send random text and have max seperate by ||| 
+oscmsg = OSC.OSCMessage()
+oscmsg.setAddress("/TestCommunication1")
+oscmsg.append("apple|||banana|||coconut")
+c.send(oscmsg)
+
+
+#send numbers (or in our case, xyz coordiantes) and have max seperate by space
+oscmsg2 = OSC.OSCMessage()
+oscmsg2.setAddress("/TestCommunication2")
+oscmsg2 += 1232
+oscmsg2 += 222
+c.send(oscmsg2)
