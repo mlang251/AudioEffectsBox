121b94df3c1a1009c7828e98632a4fff295e8661
nknaian <nickknaian@gmail.com>
2016-11-06 22:53:10 -0500

    using the oscpack library, modified the leap file so that the coordinates will be sent over a udp socket in the osc format to port 7000 to the loopback address. Haven't tested out yet bec don't have the leap. But the transmit in the main func at the beginning works. NOTE: you will need to download the oscpack library and set up some things in the project environment to get this to work

--- Leap/LeapHandTracking.cpp
+++ Leap/LeapHandTracking.cpp
@@ -2,6 +2,13 @@
 #include <string.h>
 #include "Leap.h"
 
+// includes and defines for osc packet sending
+#include "osc/OscOutboundPacketStream.h"
+#include "ip/UdpSocket.h"
+#define ADDRESS "127.0.0.1"
+#define PORT 7000
+#define OUTPUT_BUFFER_SIZE 1024
+
 // Subclass to listen for leap motion events
 class LeapEventListener : public Leap::Listener
 {
@@ -9,6 +16,11 @@ public:
     // Overwrite Listener event callback functions
     virtual void onConnect(const Leap::Controller&);
     virtual void onFrame(const Leap::Controller&);
+	void setTransmitSocket(UdpTransmitSocket* transmitSocket);
+	void setOutputPacket(osc::OutboundPacketStream* packet);
+private:
+	UdpTransmitSocket* listenerTransmitSocket; //udp transmit socket for listener
+	osc::OutboundPacketStream* listenerOutputPacket; //buffer used to send coordinates
 };
 
 // This callback function will be called once the leap motion is conencted
@@ -36,11 +48,32 @@ void LeapEventListener::onFrame(const Leap::Controller& controller)
         std::cout << int(palmPosition[1]);
         std::cout << " ";
         std::cout << int(palmPosition[2]) << std::endl;
+
+		//send packet with coordinates through udp transmit socket
+		*listenerOutputPacket << osc::BeginBundleImmediate
+			<< osc::BeginMessage("/xCoord")
+			<< palmPosition[0] << osc::EndMessage
+			<< osc::BeginMessage("/yCoord")
+			<< palmPosition[1] << osc::EndMessage
+			<< osc::BeginMessage("/zCoord")
+			<< palmPosition[2] << osc::EndMessage
+			<< osc::EndBundle;
+		listenerTransmitSocket->Send(listenerOutputPacket->Data(), listenerOutputPacket->Size());
     }
 //    float framesPerSec;
 //    framesPerSec = frame.currentFramesPerSecond();
 //    std::cout << framesPerSec << std::endl;
 
+}
+
+void LeapEventListener::setTransmitSocket(UdpTransmitSocket* transmitSocket)
+{
+	listenerTransmitSocket = transmitSocket;
+}
+
+void LeapEventListener::setOutputPacket(osc::OutboundPacketStream* packet)
+{
+	listenerOutputPacket = packet;
 }
 
 int main()
@@ -51,7 +84,23 @@ int main()
     
     // Add the listener to the controller to track new frames in another thread
     controller.addListener(listener);
-    
+
+	// open udp socket, pass to listener
+	UdpTransmitSocket transmitSocket(IpEndpointName(ADDRESS, PORT));
+	listener.setTransmitSocket(&transmitSocket);
+
+	//create buffer, pass to listener
+	char buffer[OUTPUT_BUFFER_SIZE];
+	osc::OutboundPacketStream packet(buffer, OUTPUT_BUFFER_SIZE);
+	listener.setOutputPacket(&packet);
+
+	//send one test bundle at the beginning
+	packet << osc::BeginBundleImmediate
+		<< osc::BeginMessage("/test1")
+		<< "hello wuld" << osc::EndMessage
+		<< osc::EndBundle;
+	transmitSocket.Send(packet.Data(), packet.Size());
+
     // Keep this process running until Enter is pressed
     std::cout << "Press Enter to quit..." << std::endl;
     std::cin.get();
