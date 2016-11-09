267d122bded6565173b7fe53c887975c53ee6653
Mark <mlang41110@yahoo.com>
2016-11-08 23:44:20 -0500

    Added files needed to start a Leap project in Python. leapHandTracking.py will verify you have the project set up correctly

---
+++ Leap/LeapPython/leapHandTracking.py
@@ -0,0 +1,37 @@
+# ___How to make this file work___ #
+
+# Use with Python 2.7 ONLY!!!
+# Make sure in the directory where this file is located, you have the following:
+#        Leap.py
+#        Leap.pyc (the compiled Python file)
+#        \x86 folder (if you are using 32-bit Python)
+#        \x64 folder (if you are using 64-bit Python)
+
+# If you are using the 32-bit version of Python 2.7
+#        Do nothing, leapHandTracking.py can run at it's current state
+# If you are using the 64-bit version of Python 2.7
+#        Open Leap.py and change line 19 "x86" to "x64"
+#        Save Leap.py
+
+# leapHandTracking.py is ready to run
+
+
+import os, sys, inspect
+src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
+print src_dir
+lib_dir = os.path.abspath(os.path.join(src_dir, '../Leap_Motion_SDK_Windows_2.3.1\LeapDeveloperKit_2.3.1+31549_win\LeapSDK\lib'))
+print lib_dir
+sys.path.insert(0, lib_dir)
+import Leap
+
+def main():
+
+    # Keep this process running until Enter is pressed
+    print "Press Enter to quit..."
+    try:
+        sys.stdin.readline()
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    main()
