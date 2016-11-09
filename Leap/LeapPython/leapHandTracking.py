d93ab23f5df0896f24e0f13b6cee41ef007fb94a
Mark <mlang41110@yahoo.com>
2016-11-09 18:02:29 -0500

    Got leapHandTracking.py working with Leap

--- Leap/LeapPython/leapHandTracking.py
+++ Leap/LeapPython/leapHandTracking.py
@@ -1,5 +1,6 @@
 # ___How to make this file work___ #
 
+# Install the Leap sofware
 # Use with Python 2.7 ONLY!!!
 # Make sure in the directory where this file is located, you have the following:
 #        Leap.py
@@ -12,7 +13,23 @@
 import sys, Leap
 
 
+class SampleListener(Leap.Listener):
+
+    def on_connect(self, controller):
+        print "Connected"
+
+
+    def on_frame(self, controller):
+        print "Frame available"
+        
+
 def main():
+    # Create a sample listener and controller
+    listener = SampleListener()
+    controller = Leap.Controller()
+
+    # Have the sample listener receive events from the controller
+    controller.add_listener(listener)
 
     # Keep this process running until Enter is pressed
     print "Press Enter to quit..."
@@ -20,6 +37,9 @@ def main():
         sys.stdin.readline()
     except KeyboardInterrupt:
         pass
+    finally:
+        # Remove the sample listener when done
+        controller.remove_listener(listener)
 
 if __name__ == "__main__":
     main()
