2251f1433f8c0047f1131b63f35768c2403484dd
Chris Babroski <cjbabroski@gmail.com>
2017-02-15 21:01:33 -0500

    Minor updates, still working on smoothing

--- Leap/ActivityListener.py
+++ Leap/ActivityListener.py
@@ -1,4 +1,5 @@
 import OSC
+from collections import deque
 from Leap import Listener
 
 class ActivityListener(Listener):
@@ -14,9 +15,13 @@ class ActivityListener(Listener):
         Establishes a connection to a remote server using UDP.
 
         """
+        # Setup OSC connection to local host port 8000
         self.data_client = OSC.OSCClient()
         self.data_client.connect(('127.0.0.1', 8000))
         print "Connected"
+
+        # Create a queue to hold Leap frame data with a set max length
+        self.frame_queue = deque(maxlen=2)
 
     def on_frame(self, controller):
         """
@@ -25,15 +30,22 @@ class ActivityListener(Listener):
         gets and sends palm position coordinates to the application server.
 
         """
+        # Store new frame
         frame = controller.frame()
         interaction_box = frame.interaction_box
 
-        # Get palm position coordinates
-        palm_position = self.get_palm_position(frame, interaction_box)
+        # Get palm position coordinates of newest frame in queue
+        palm_position = self.get_palm_position(frame)
+
+        # Normalize hand position within the Leap interaction box (0-1 for xyz)
+        normalized_palm = interaction_box.normalize_point(palm_position, True)
+
+        # Set pinch threshold: number from 0-1 describing pinch strength
+        pinch_threshold = 0.7
 
         # If user is pinching, send xyz palm coordinates to server
-        if frame.hands[0].pinch_strength > 0.5:
-            self.send_palm_position(palm_position)
+        if frame.hands[0].pinch_strength > pinch_threshold:
+            self.send_palm_position(normalized_palm)
 
     def send_palm_position(self, palm_position):
         """
@@ -57,12 +69,12 @@ class ActivityListener(Listener):
         # Use OSC to send xyz coordiantes and have max seperate by space
         self.oscmsg = OSC.OSCMessage()
         self.oscmsg.setAddress("/Coordinates")
-        self.oscmsg += round(normalized.x, 3)
-        self.oscmsg += round(normalized.y, 3)
-        self.oscmsg += round(normalized.z, 3)
+        self.oscmsg += round(palm_position.x, 3)
+        self.oscmsg += round(palm_position.y, 3)
+        self.oscmsg += round(palm_position.z, 3)
         self.data_client.send(self.oscmsg)
 
-    def get_palm_position(self, frame, interaction_box):
+    def get_palm_position(self, frame):
         """
         Gets normalized palm position coordinates.
         This function also smoothens out palm position data when a user's hand
@@ -82,19 +94,23 @@ class ActivityListener(Listener):
         # Track first hand available
         hand = frame.hands[0]
 
-        # Normalize hand position within the Leap interaction box (0-1 for xyz)
-        normalized_palm = interaction_box.normalize_point(hand.palm_position, True)
-
         # If there is a hand, return normalized palm position
         if not frame.hands.is_empty:
-            return normalized_palm
+            # Add good frame to the frame queue
+            self.frame_queue.append(frame)
+            return hand.palm_position
 
         # If there is no hand, it may have left the interaction box for a
         # moment and new data will have to be smoothened
         else:
-            return normalized_palm #self.smooth_palm_data(...)
+            print "OUT OF BOUNDS"
 
-    def smooth_palm_data(self, frame):
+            # Wait for new frame of hand data that is inside interaction box
+            # and use last good frame to calculate and return a midpoint
+            # as hand position
+            last_good_frame = self.frame_queue.pop()
+
+    def smooth_palm_data(self, last_frame):
         """
         Basic algorithm would have to wait for a new frame of data,
         calculate new coordinates and return a coordinate in between the old and new
@@ -103,7 +119,7 @@ class ActivityListener(Listener):
         May need to include frame.hands.is_empty in this function instead of
         inside get_palm_position()
 
-        1. Pass in old frame
+        1. Pass in previous frame
         2. Wait for new frame
         3. Return midpoint between last frame and new frame
 
