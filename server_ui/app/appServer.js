bcb10f27ad70884b64e16eef67d6c0759d57c769
Mark <mlang41110@yahoo.com>
2017-02-16 19:29:27 -0500

    added new Leap to Server OSC socket

--- server_ui/app/appServer.js
+++ server_ui/app/appServer.js
@@ -28,14 +28,23 @@ const serverToMaxChannel = {
     portXYZMap: new OscUdpPort({remotePort: 7040, address: "xyzMap"})              //For assigning x, y, and z to specific effect parameters
 };
 
-//Create the Leap --> Server OSC socket
-const leapToServerChannel = new OscUdpPort({localPort: 8000});
+//Create the Leap --> Server OSC sockets
+const leapToServerChannel = {
+    portLeapCoords: new OscUdpPort({localPort: 8000}),
+    portLeapErrors: new OscUdpPort({localPort: 8010})
+};
 
-leapToServerChannel.on("message", msg => {
+leapToServerChannel.portLeapCoords.on("message", msg => {
     const data = msg.args;
     console.log(`received message from leap: ${data}`);
     serverToMaxChannel.portLeapCoords.sendData(data);
     io.emit('leapData', data);
+});
+
+leapToServerChannel.portLeapErrors.on('message', msg => {
+    const data = msg.args;
+    console.log(`received message from leap: ${data}`);
+    io.emit('leapError', data);
 });
 
 
