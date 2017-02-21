b3cdf23f75b1470fb0cc06a078823c27aebb8050
Mark <mlang41110@yahoo.com>
2017-02-21 12:40:53 -0500

    added hatch pattern to InteractionBox

--- server_ui/app/components/InteractionBox.js
+++ server_ui/app/components/InteractionBox.js
@@ -40,41 +40,55 @@ const styles = {
         height: '100%',
         position: 'absolute',
         transformStyle: 'preserve-3d',
-        transform: 'translateZ(-150px) rotateX(-20deg)'
+        transform: 'translateZ(-100px) rotateX(-20deg)'
     },
     figure: {
         margin: 0,
-        width: 296,
-        height: 296,
+        width: '100%',
+        height: '100%',
         display: 'block',
         position: 'absolute',
         borderWidth: 2,
         borderStyle: 'solid',
         borderColor: 'black',
-        opacity: '0.5'
+        backgroundImage: `linear-gradient(0deg, transparent 45%, #009 50%, transparent 55%, transparent 100%), 
+            linear-gradient(90deg, transparent 45%, #009 50%, transparent 55%, transparent 100%)`,
+        opacity: 0.5
     },
     front: {
-        transform: 'rotateY(0deg) translateZ(150px)',
-        backgroundColor: '#800'
+        transform: 'rotateY(0deg) translateZ(100px)',
+        backgroundColor: '#FFF',
+        backgroundImage: '',
+        opacity: 0.2
     },
     back: {
-        transform: 'rotateX(180deg) translateZ(150px)',
-        backgroundColor: '#F00'
+        transform: 'rotateX(180deg) translateZ(100px)',
+        backgroundSize: '10px 10px',
+        backgroundPosition: '5px 5px',
+        backgroundColor: '#AAA'
     },
     right: {
-        transform: 'rotateY(90deg) translateZ(150px)',
-        backgroundColor: '#080'
+        transform: 'rotateY(90deg) translateZ(100px)',
+        backgroundSize: '10px 10px',
+        backgroundPosition: '5px 5px',
+        backgroundColor: '#AAA'
     },
     left: {
-        transform: 'rotateY(-90deg) translateZ(150px)',
-        backgroundColor: '#0F0'
+        transform: 'rotateY(-90deg) translateZ(100px)',
+        backgroundSize: '10px 10px',
+        backgroundPosition: '5px 5px',
+        backgroundColor: '#AAA'
     },
     top: {
-        transform: 'rotateX(90deg) translateZ(150px)',
-        backgroundColor: '#008'
+        transform: 'rotateX(90deg) translateZ(100px)',
+        backgroundColor: '#EEE',
+        backgroundImage: '',
+        opacity: 0.2
     },
     bottom: {
-        transform: 'rotateX(-90deg) translateZ(150px)',
-        backgroundColor: '#00F'
+        transform: 'rotateX(-90deg) translateZ(100px)',
+        backgroundSize: '20px 20px',
+        backgroundPosition: '10px 10px',
+        backgroundColor: '#EEE'
     },
 }
