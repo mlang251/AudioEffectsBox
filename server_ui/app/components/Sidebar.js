2a9f78e90f421547057cdcf65ca6c177a10061ac
Mark <mlang41110@yahoo.com>
2017-03-15 04:35:51 -0400

    finished JSDoc for Sidebar component

--- server_ui/app/components/Sidebar.js
+++ server_ui/app/components/Sidebar.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import Radium from 'radium';
 
+/**
+ * The Sidebar module. Responsible rendering a Sidebar component which is used for adding effects to the signal
+ *     chain and for entering mapping mode. Appears as a child component of the SidebarContainer component. 
+ * @module Sidebar
+ * @see module:SidebarContainer
+ */
+
+/** 
+ * Class responsible rendering the Sidebar component.
+ * @extends external:ReactPureComponent 
+ */
 class Sidebar extends React.PureComponent {
+    /** Create the Sidebar component instance */
     constructor() {
         super();
     }
 
+    /**
+     * Renders HTML for the Sidebar component
+     */
     render() {
         return (
             <div>
@@ -18,4 +33,5 @@ class Sidebar extends React.PureComponent {
     }
 }
 
+/** The Sidebar component */
 export default Radium(Sidebar);
