3e230025d5344747193b803a8c6675eee7ce9ae6
Mark <mlang41110@yahoo.com>
2017-03-16 23:58:00 -0400

    added new function to effects reducer

--- server_ui/app/reducers/effects.js
+++ server_ui/app/reducers/effects.js
@@ -1,10 +1,35 @@
 import {List, Map} from 'immutable';
 import {ADD_EFFECT, REMOVE_EFFECT, REORDER_EFFECTS} from '../actions/actions';
 
+const addEffect = (state) => {
+    const usableIDs = this.effects.getIn(['effects', effectType, 'IDs']);
+    const usedIDs = state.map(effect => effect.ID);
+    usableIDs.forEach((curID, index) => {
+        if (usedIDs.includes(curID)) {
+            if (index == usableIDs.size - 1) {
+                alert(`Maximum number of ${effectType} effects reached.`);
+            }
+        } else {
+            const newEffectsArray = this.state.effects.push(Immutable.fromJS({
+                type: effectType,
+                ID: curID,
+                isBypassed: false,
+                isSoloing: false
+            }));
+            this.setState(({usedIDs, effects}) => ({
+                usedIDs: usedIDs.push(curID).sort(),
+                effects: newEffectsArray
+            }));
+            this.createRoutes(newEffectsArray);
+            return false;
+        }
+    });
+}
+
 const effects = (state = List(), action) => {
     switch (action.type) {
         case ADD_EFFECT:
-            return state.
+            return state.push(addEffect(state));
         case REMOVE_EFFECT:
         case REORDER_EFFECTS:
         default:
