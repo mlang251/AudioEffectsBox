720e59a2df6a627865ba0310d103ff2d182ca292
Mark <mlang41110@yahoo.com>
2017-03-20 23:03:46 -0400

    updated reducer for emit

--- server_ui/app/reducers/emit.js
+++ server_ui/app/reducers/emit.js
@@ -3,9 +3,13 @@ import {Map} from 'immutable';
 const emit = (state = Map(), action) => {
     switch(action.type){
         case 'route':
+            return Map({route: action.data});
+            break;
         case 'updateParam':
+            return Map({updateParam: action.data});
+            break;
         case 'xyzMap':
-            return Map(action.data);
+            return Map({xyzMap: action.data});
             break;
         default:
             return state;
