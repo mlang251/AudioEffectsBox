e426980676f17e4ea44fe695c890cfe363ef579f
Mark <mlang41110@yahoo.com>
2017-03-23 23:08:46 -0400

    made Redux SignalChainContainer

---
+++ server_ui/app/containers/SignalChainContainer.js
@@ -0,0 +1,35 @@
+import {connect} from 'react-redux';
+import {toggleMapping, updateParameterValue, mapToParameter, removeMapping} from '../actions/actionCreators';
+import Parameter from '../components/Parameter';
+
+const mapStateToProps = state => {
+    return {
+        parameterValues: state.parameterValues,
+        mapping: state.mapping,
+        xyzMap: state.xyzMap
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        toggleMapping: axis => {
+            dispatch(toggleMapping(axis));
+        },
+        updateParameterValue: (effectID, paramName, paramValue) => {
+            dispatch(updateParameterValue(effectID, paramName, paramValue));
+        }, 
+        mapToParameter: (effectID, paramName, axis) => {
+            dispatch(mapToParameter((effectID, paramName, axis)));
+        },
+        removeMapping: axis => {
+            dispatch(removeMapping(axis));
+        }
+    };
+};
+
+const ParameterContainer = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Parameter);
+
+export default ParameterContainer;
