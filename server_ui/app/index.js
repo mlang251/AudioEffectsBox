077aca4ea2835f775988f7e5d50e274f02899b76
Mark <mlang41110@yahoo.com>
2017-03-29 14:53:57 -0400

    integrated redux-socket.io

--- server_ui/app/index.js
+++ server_ui/app/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import createSocketIoMiddleware from 'redux-socket.io';
+import io from 'socket.io-client';
 import app from './reducers/app';
 import AppContainer from './ReduxComponents/AppContainer';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
+const socket = io('http://localhost:3000');
+const socketIoMiddleware = createSocketIoMiddleware(socket, (type, action) => action.options.io);
+const store = applyMiddleware(socketIoMiddleware)(createStore)(app);
+
 injectTapEventPlugin();
 
 render(
     <MuiThemeProvider>
-        <Provider store = {createStore(app)}>
+        <Provider store = {store}>
             <AppContainer />
         </Provider>
     </MuiThemeProvider>,
