9726544ef78d1116a40f2658d66839d3f2f6ef6a
Mark <mlang41110@yahoo.com>
2017-04-01 16:28:39 -0400

    added TODO comment

--- server_ui/app/ReduxComponents/SidebarContainer.js
+++ server_ui/app/ReduxComponents/SidebarContainer.js
@@ -5,7 +5,11 @@ import {effects} from '../JSON/effects.json';
 const {ROUTE} = require('../actions/actionOptions').ioTypes;
 const {ADD} = require('../actions/actionOptions').ioFlags;
 
-const checkUsedIDs = (effectType, usedIDs, dispatch) => {
+/*TODO: use Thunk to handle an action creator that is a function that passes the effects state slice to the dispatch method.
+    take the list of effects, add the new on to the list, and dispatch the computed list to the store, and in the options section
+    include the route to emit, or have the server compute the route (probably this is better so there can be a single function
+    that checks for soloing/bypassed effects and puts the whole list in the proper format*/
+const checkUsedIDs = (effectType, usedIDs, effectsRoute, dispatch) => {
     const usableIDs = effects[effectType].IDs;
     for (let i = 0; i < usableIDs.length; i++) {
         const thisID = usableIDs[i];
@@ -17,7 +21,8 @@ const checkUsedIDs = (effectType, usedIDs, dispatch) => {
             dispatch(addEffect(effectType, thisID, {
                 io: true,
                 ioType: ROUTE,
-                ioFlag: ADD
+                ioFlag: ADD,
+                currentRoute: effectsRoute
             }));
             break;
         }
@@ -30,7 +35,7 @@ const mapDispatchToProps = (dispatch, ownProps) => {
             dispatch(updateMapping(false, axis));
         },
         addEffect: (effectType) => {
-            checkUsedIDs(effectType, ownProps.usedIDs, dispatch);
+            checkUsedIDs(effectType, ownProps.usedIDs, ownProps.effectsRoute, dispatch);
         }
     };
 };
