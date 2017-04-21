f31c9d638b6710637a8cce5c9c327fd61e5c799e
Mark <mlang41110@yahoo.com>
2017-04-21 18:13:11 -0400

    updated package.json and main.js

--- server_ui/app/main.js
+++ server_ui/app/main.js
@@ -11,7 +11,6 @@ app.on('window-all-closed', () => {
 app.on('ready', () => {
     win = new BrowserWindow({width: 1360, height: 800});
     win.loadURL('file://' + __dirname + '/public/index.html');
-    win.webContents.openDevTools();
     win.on('closed', function() {
         win = null;
     });
